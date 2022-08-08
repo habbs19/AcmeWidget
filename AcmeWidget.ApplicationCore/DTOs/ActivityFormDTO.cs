3e070d07c94ebe397d9b7d396f5b248a24c63003
Habib Shakibanejad <hs_m19@hotmail.com>
2022-08-07 22:02:15 -0400

    formDTO

--- AcmeWidget.ApplicationCore/DTOs/ActivityFormDTO.cs
+++ AcmeWidget.ApplicationCore/DTOs/ActivityFormDTO.cs
@@ -1,6 +1,7 @@
 ﻿using AcmeWidget.ApplicationCore.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,11 @@ namespace AcmeWidget.ApplicationCore.DTOs
     public class ActivityFormDTO
     {
         public int? FormId { get; set;}
+
+        [Required]
         public EmployeeDTO Employee { get; set; } = null!;
+
+        [Required]
         public ActivityDTO Activity { get; set; } = null!;
         public string Comments { get; set; } = null!;
     }
